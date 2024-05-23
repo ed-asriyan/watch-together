84e4bb402c8e0e444af17de9c28c77033cb6688f
Ed Asriyan <github@asriyan.me>
2024-05-23 13:34:52 +0400

    Add highlight

--- src/components/room.svelte
+++ src/components/room.svelte
@@ -11,11 +11,16 @@
     import { randomStr } from '../utils';
     import { track, ClickEvent } from '../analytics.svelte';
     import { isExample } from '../stores/video-example';
+    import { blobUrl } from '../stores/blob';
 
     export let roomId: string;
     export let room: Room;
 
     $: users = room?.users;
+
+    $: url = room?.url;
+    $: highlightVideoSelector = room && !$url && !$blobUrl;
+    $: highlightInvite = room && !highlightVideoSelector && $users?.length < 1;
 
     const updateRoom = function (newRoomId: string, copyData?: boolean) {
         document.location.hash = `#${newRoomId}`;
@@ -53,15 +58,19 @@
 </div>
 <div class="uk-container uk-grid-collapse uk-grid-match uk-margin-top" uk-grid>
     <div class="uk-width-1-2@m uk-padding-small">
-        <div class="tile">
-            <h2 class="uk-card-title uk-text-center">🍿 { $_('selectVideo.title') }</h2>
-            <CardVideoSelector room={room} />
+        <div class:gradient-border={highlightVideoSelector}>
+            <div class="tile uk-width-1-1">
+                <h2 class="uk-card-title uk-text-center">🍿 { $_('selectVideo.title') }</h2>
+                <CardVideoSelector room={room} />
+            </div>
         </div>
     </div>
     <div class="uk-width-1-2@m uk-padding-small">
-        <div class="tile uk-text-center">
-            <h2 class="uk-card-title">👥 { $_('invite.title') }</h2>
-            <CardCopyUrl roomId={roomId} />
+        <div class:gradient-border={highlightInvite}>
+            <div class="tile uk-text-center uk-width-1-1">
+                <h2 class="uk-card-title">👥 { $_('invite.title') }</h2>
+                <CardCopyUrl roomId={roomId} />
+            </div>
         </div>
         <div class="uk-width-1-1 uk-margin-top">
             <Users users={$users} />
@@ -94,5 +103,44 @@
         width: 100vw;
         min-height: min(calc(100vw * 9 / 16), 65vh);
         max-height: min(calc(100vw * 9 / 16), 65vh);
+    }
+
+    @keyframes moveGradient {
+        50% {
+            background-position: 100% 100%;
+            opacity: 60%;
+        }
+    }
+
+    $border-width: 1px;
+    .gradient-border {
+        position: relative;
+        display: flex;
+        z-index: 0;
+    
+        &::after {
+            position: absolute;
+            content: "";
+            top: -$border-width;
+            left: -$border-width;
+            z-index: -1;
+            width: calc(100% + $border-width * 2);
+            height: calc(100% + $border-width * 2);
+            background: linear-gradient(
+                60deg,
+                hsl(224, 85%, 66%),
+                hsl(269, 85%, 66%),
+                hsl(314, 85%, 66%),
+                hsl(359, 85%, 66%),
+                hsl(44, 85%, 66%),
+                hsl(89, 85%, 66%),
+                hsl(134, 85%, 66%),
+                hsl(179, 85%, 66%),
+            );
+            background-size: 300% 300%;
+            background-position: 0 0;
+            border-radius: 1rem;
+            animation: moveGradient 2s linear infinite;
+        }
     }
 </style>
