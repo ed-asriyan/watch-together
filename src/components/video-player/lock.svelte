bf78e86f7b56846b640e6eca9e17368af2864a9b
Ed Asriyan <github@asriyan.me>
2024-08-22 18:23:24 -0700

    Add lock buttons

---
+++ src/components/video-player/lock.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    export let locked: boolean;
+</script>
+
+<span class="lock pointer" on:click={() => locked = !locked }>
+    {#if locked}
+        🔒
+    {:else}
+        🔓
+    {/if}
+</span>
+
+<style lang="scss">
+    .lock {
+        z-index: 101;
+        opacity: 0.7;
+
+        transition: opacity 100ms ease-in-out;
+
+        &:hover {
+            opacity: 1;
+        }
+    }
+</style>
